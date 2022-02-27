21f3c968aabb13f51bd1b67b397f744ca8a08b67
John Bruce <jbruce2112@gmail.com>
2022-02-27 15:04:27 -0800

    Initial Commit

---
+++ SpaceY/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpaceY
+//
+//  Created by John on 2/27/22.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
